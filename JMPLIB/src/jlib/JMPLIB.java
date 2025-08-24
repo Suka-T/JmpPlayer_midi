0d7edc8dd5d9563437c42a9ff335434be9fad4bd
Suka-T <absolute6626x@gmail.com>
2025-08-24 12:34:45 +0900

    R1.03発行

--- JMPLIB/src/jlib/JMPLIB.java
+++ JMPLIB/src/jlib/JMPLIB.java
@@ -3,7 +3,7 @@ package jlib;
 public class JMPLIB {
 
     /** ビルドバージョン */
-    public static final String BUILD_VERSION = "1.02";
+    public static final String BUILD_VERSION = "1.03";
     
     /* バージョン名 */
     public static final String VERSION_NAME = "";
