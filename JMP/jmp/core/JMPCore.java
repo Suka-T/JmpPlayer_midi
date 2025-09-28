64d3b5cf2f05f683acbb63cd98b35ba299404d43
Suka-T <absolute6626x@gmail.com>
2025-09-28 19:32:06 +0900

    ver1.04.001

--- JMP/jmp/core/JMPCore.java
+++ JMP/jmp/core/JMPCore.java
@@ -21,7 +21,7 @@ public class JMPCore {
     public static final String APPLICATION_NAME = "JMPPlayer";
 
     /** アプリケーションバージョン */
-    public static final String APPLICATION_VERSION = "1.03.003"; /*
+    public static final String APPLICATION_VERSION = "1.04.001"; /*
                                                                   * メジャーリリース番号.
                                                                   * 機能追加番号.修正番号
                                                                   */
--- JMPLIB/src/jlib/JMPLIB.java
+++ JMPLIB/src/jlib/JMPLIB.java
@@ -3,7 +3,7 @@ package jlib;
 public class JMPLIB {
 
     /** ビルドバージョン */
-    public static final String BUILD_VERSION = "1.03";
+    public static final String BUILD_VERSION = "1.04";
     
     /* バージョン名 */
     public static final String VERSION_NAME = "";
