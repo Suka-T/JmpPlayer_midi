02045b46eef4dffd9bcfee21460349bff75c7991
Suka-T <absolute6626x@gmail.com>
2026-04-06 01:15:14 +0900

    成果物出力bat追加

--- JmpPlayer_Gradle/make.bat
+++ JmpPlayer_Gradle/make.bat
@@ -3,7 +3,14 @@ call .\gradlew.bat :jmpp:fatJar
 mkdir JMPPlayer_vX.XX
 
 copy .\jmpp\build\libs\JMPPlayer-1.0.jar .\JMPPlayer_vX.XX\JMPPlayer.jar
-xcopy .\jmpp\bin\skin .\JMPPlayer_vX.XX\skin /S /E /I /H /Y
-copy .\jmpp\bin\*.exe .\JMPPlayer_vX.XX
+xcopy ..\jre .\JMPPlayer_vX.XX\jre /S /E /I /H /Y
+xcopy ..\skin .\JMPPlayer_vX.XX\skin /S /E /I /H /Y
+copy ..\clean.bat .\JMPPlayer_vX.XX
+copy ..\clean_all.bat .\JMPPlayer_vX.XX
+copy ..\JMPPlayer.bat .\JMPPlayer_vX.XX
+copy ..\JMPPlayer_Unload_plugin.bat .\JMPPlayer_vX.XX
+copy ..\JMPPlayer.exe .\JMPPlayer_vX.XX
+copy ..\ffmpeg.exe .\JMPPlayer_vX.XX
+copy ..\ffprobe.exe .\JMPPlayer_vX.XX
 
 pause
