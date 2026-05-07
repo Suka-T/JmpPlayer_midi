13a3bbf2d0008b80f7fac8ffd2201406ebcf4511
Suka-T <absolute6626x@gmail.com>
2026-05-08 00:27:21 +0900

    v1.10.001

--- JmpPlayer_Gradle/jmplib/src/jlib/JMPLIB.java
+++ JmpPlayer_Gradle/jmplib/src/jlib/JMPLIB.java
@@ -3,7 +3,7 @@ package jlib;
 public class JMPLIB {
 
     /** ビルドバージョン */
-    public static final String BUILD_VERSION = "1.07";
+    public static final String BUILD_VERSION = "1.10"; // JMPCoreのバージョンと合わせる 
     
     /* バージョン名 */
     public static final String VERSION_NAME = "";
